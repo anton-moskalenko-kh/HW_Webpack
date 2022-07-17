67233d713f8ba93c95abe3544297006635515b7e
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2022-07-17 20:07:54 +0300

    HW_Webpack_ver.1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        main: [
+            path.join(__dirname, "./src/js/index.js"),
+            path.join(__dirname, "./src/scss/style.scss"),
+        ]
+    },
+    devtool: "source-map",
+    output: {
+        filename: "index.min.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    miniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "./index.html"}),
+        new miniCssExtractPlugin()
+    ]
+}
